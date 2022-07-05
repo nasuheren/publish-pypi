0f213b630b103dfe189efed146419c5801c53c15
nasuheren <nasuherendemirci@gmail.com>
2022-07-05 20:43:42 +0300

    deneme

--- src/mypackage/job.py
+++ src/mypackage/job.py
@@ -1,29 +1,28 @@
-from unicodedata import name
-from kivy.app import App
-from kivy.uix.label import Label
-import json
+#from kivy.app import App
+#from kivy.uix.label import Label
+#import json
 
 
 # Ekrana yazi yazar
-class Yazbel(App):
-    def build(self):
-        yazi = Label(text = "Hello :D",font_size = "25sp",color = [0,.3,.9,1])
-        return yazi
+#class Yazbel(App):
+#    def build(self):
+#        yazi = Label(text = "Hello :D",font_size = "25sp",color = [0,.3,.9,1])
+#        return yazi
 
-Yazbel().run()
+#Yazbel().run()
 
-
+print("Merhaba")
 
 # Deger girilip toplamaya yarar ve Json dosyasi olarak dondurur
 
-def topla():
-    a = int(input("Deger giriniz"))
-    b = int(input("Deger giriniz"))
-    c = a + b
-    x = {
-      "name": "Nasuh Eren Demirci",
-      "sonuc": c
-    }
-    y = json.dumps(x)
-    print(y)
-topla()
+#def topla():
+#    a = int(input("Deger giriniz"))
+#    b = int(input("Deger giriniz"))
+#    c = a + b
+#    x = {
+#      "name": "Nasuh Eren Demirci",
+#      "sonuc": c
+#    }
+#    y = json.dumps(x)
+#    print(y)
+#topla()
